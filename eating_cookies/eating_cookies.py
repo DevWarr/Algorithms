2d5d808ca5bd8b9b964ccb49a01eac07c931ba39
Devin Warrick <dev.warr@gmail.com>
2020-04-15 16:32:32 -0500

    Adds cache to eating_cookies to pass large tests

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -12,16 +12,44 @@ def eating_cookies(n, cache=None):
     if n == 0:
         # If zero, return 1.
         return 1
+
     if n < 0:
+        # If less than 0, return 0.
         return 0
-    # If less than 0, return 0.
+
+    if cache is None:
+        # No cache → create one
+        cache = [0] * (n+1)
 
     # To get all of the possible values
     # We can subtract 1, 2, or 3,
     #     and then call this funciton again.
     # Once we hit zero,
     #     we return 1 and boomerang back up the stack.
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    # BUT, instead of creating a thousand recursive calls,
+    # Let's store our values in a cache as we go.
+
+    # Check our cache for the values minus 1, 2, and 3
+    minus_3 = cache[n-3]
+    minus_2 = cache[n-2]
+    minus_1 = cache[n-1]
+    # ==============================================
+    #  NOTE:
+    # If we get values -1 or -2 (2-3 or 1-3), 
+    #     our cache will still return 0.
+    # That's because we don't know (yet) what n or n-1 are
+    # Until the function's almost done boomeranging back up the stack.
+    # ==============================================
+
+    # If our cache doesn't have the proper value (It should never be zero)
+    if minus_3 == 0:
+        minus_3 = cache[n-3] = eating_cookies(n-3, cache)
+    if minus_2 == 0:
+        minus_2 = cache[n-2] = eating_cookies(n-2, cache)
+    if minus_1 == 0:
+        minus_1 = cache[n-1] = eating_cookies(n-1, cache)
+
+    return minus_1 + minus_2 + minus_3
 
 
 if __name__ == "__main__":
